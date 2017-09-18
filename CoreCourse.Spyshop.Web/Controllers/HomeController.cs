f0a226aaee4cf1ece6fcb4ff67d522877dbbf5e6
sigged <sderdeyn@gmail.com>
2017-09-18 18:47:02 +0200

    Added Mvc Middleware

---
+++ CoreCourse.Spyshop.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCourse.Spyshop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Mvc!");
+        }
+    }
+}
