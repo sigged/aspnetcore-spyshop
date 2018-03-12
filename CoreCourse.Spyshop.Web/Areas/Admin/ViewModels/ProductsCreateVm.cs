b6b200f9ffd3538165a1a04c7784ffedd75e8b1f
sigged <sderdeyn@gmail.com>
2018-03-12 19:32:31 +0100

    Refactored ProductAdmin's Index, Edit and Create methods to use View Models

---
+++ CoreCourse.Spyshop.Web/Areas/Admin/ViewModels/ProductsCreateVm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreCourse.Spyshop.Web.Areas.Admin.ViewModels
+{
+    public class ProductsCreateVm
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        [MaxLength(250)]
+        public string PhotoUrl { get; set; }
+
+        public int? SortNumber { get; set; }
+    }
+}
