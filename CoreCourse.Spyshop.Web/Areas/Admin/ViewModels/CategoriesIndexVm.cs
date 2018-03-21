0515771daad8870c4c719086ca7215bab80a9ce9
sigged <sderdeyn@gmail.com>
2018-03-21 15:32:56 +0100

    Added category management to admin area

---
+++ CoreCourse.Spyshop.Web/Areas/Admin/ViewModels/CategoriesIndexVm.cs
@@ -0,0 +1,10 @@
+﻿using CoreCourse.Spyshop.Domain.Catalog;
+using System.Collections.Generic;
+
+namespace CoreCourse.Spyshop.Web.Areas.Admin.ViewModels
+{
+    public class CategoriesIndexVm
+    {
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
