e2c4766fc2b003d8a60d42ea81af881a5bde9f8b
sigged <sderdeyn@gmail.com>
2018-03-28 01:17:13 +0200

    Added product detail page (exercise solution)

--- CoreCourse.Spyshop.Web/Controllers/CatalogController.cs
+++ CoreCourse.Spyshop.Web/Controllers/CatalogController.cs
@@ -4,6 +4,7 @@ using CoreCourse.Spyshop.Web.Data;
 using CoreCourse.Spyshop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreCourse.Spyshop.Web.Controllers
@@ -11,10 +12,14 @@ namespace CoreCourse.Spyshop.Web.Controllers
     public class CatalogController : Controller
     {
         private readonly IRepository<Category, long> _cRepository;
+        private readonly IRepository<Product, long> _pRepository;
 
-        public CatalogController(IRepository<Category, long> cRepository)
+        public CatalogController(
+            IRepository<Category, long> cRepository,
+            IRepository<Product, long> pRepository)
         {
             this._cRepository = cRepository;
+            this._pRepository = pRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -28,5 +33,22 @@ namespace CoreCourse.Spyshop.Web.Controllers
 
             return View(viewmodel);
         }
+
+        [Route("~/Catalog/Product/{id:long}/{*name}")]
+        public async Task<IActionResult> Product(long? id, string name)
+        {
+            if (id.HasValue)
+            {
+                var vm = new CatalogProductVm();
+                vm.Product = await _pRepository.GetByIdAsync(id.Value);
+                if (vm.Product != null && 
+                    vm.Product.Name?.ToLower().Trim() == name?.ToLower().Trim())
+                {
+                    return View(vm);
+                }
+            }
+            return NotFound();
+        }
+        
     }
 }
---
+++ CoreCourse.Spyshop.Web/ViewModels/CatalogProductVm.cs
@@ -0,0 +1,9 @@
+﻿using CoreCourse.Spyshop.Domain.Catalog;
+
+namespace CoreCourse.Spyshop.Web.ViewModels
+{
+    public class CatalogProductVm
+    {
+        public Product Product { get; set; }
+    }
+}
