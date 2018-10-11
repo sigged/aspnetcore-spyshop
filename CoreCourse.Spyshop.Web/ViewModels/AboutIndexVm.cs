1d470b5a38e8183c4deaed331c34c86c52d348be
sigged <sderdeyn@gmail.com>
2018-10-12 00:48:54 +0200

    Demonstrate usage of View Model on About/Index

---
+++ CoreCourse.Spyshop.Web/ViewModels/AboutIndexVm.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreCourse.Spyshop.Web.ViewModels
+{
+    public class AboutIndexVm
+    {
+        public string ContactEmail { get; set; }
+        public string CompanyFullName { get; set; }
+        public string AboutTitle { get; set; }
+        public string AboutContent { get; set; }
+    }
+}
