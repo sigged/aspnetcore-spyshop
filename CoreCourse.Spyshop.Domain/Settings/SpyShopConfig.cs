a8605e70e8ceb387bf0e01dafe406879f260a394
sigged <sderdeyn@gmail.com>
2018-10-12 02:12:06 +0200

    Added JSON configuration files and their support types

---
+++ CoreCourse.Spyshop.Domain/Settings/SpyShopConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreCourse.Spyshop.Domain.Settings
+{
+    public class SpyShopConfig
+    {
+        public string FullCompanyName { get; set; }
+        public SpyShopMailSettings MailSettings { get; set; }
+    }
+
+    public class SpyShopMailSettings
+    {
+        public string PublicInfoAddress { get; set; }
+        public int MailSenderRetries { get; set; }
+        public string[] AdditionalInfoAddresses { get; set; }
+    }
+}
--- CoreCourse.Spyshop.Web/Controllers/AboutController.cs
+++ CoreCourse.Spyshop.Web/Controllers/AboutController.cs
@@ -1,18 +1,29 @@
-﻿using CoreCourse.Spyshop.Web.ViewModels;
+﻿using CoreCourse.Spyshop.Domain.Settings;
+using CoreCourse.Spyshop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace CoreCourse.Spyshop.Web.Controllers
 {
     public class AboutController : Controller
     {
+        //holds the options under the SpyShopConfig configurationnode
+        private SpyShopConfig spyShopConfig;
+
+        //constructor using dependency injection to get our specific options
+        public AboutController(IOptionsSnapshot<SpyShopConfig> spyshopconfig)
+        {
+            spyShopConfig = spyshopconfig.Value;
+        }
+
         public IActionResult Index()
         {
             //Create the View Model
             var viewModel = new AboutIndexVm();
 
             //Populate View Model
-            viewModel.ContactEmail = "info@spysphop.example";
-            viewModel.CompanyFullName = "Spy Shop Incorporated";
+            viewModel.ContactEmail = spyShopConfig.MailSettings.PublicInfoAddress;
+            viewModel.CompanyFullName = spyShopConfig.FullCompanyName;
             viewModel.AboutTitle = "Welcome to Spy Shop";
             viewModel.AboutContent = "<p>We deliver premium gadgets to help all Clouseaus and Bonds out there.<br />To start, have a look at the <a href=\"/\">homepage</a>!</p>";
 
