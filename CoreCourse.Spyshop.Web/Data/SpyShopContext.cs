ba0756682112a4f4cdb9a51d478bbf415f240ff5
sigged <sderdeyn@gmail.com>
2018-10-14 23:17:07 +0200

    Scaffold admin products controller

--- CoreCourse.Spyshop.Web/Data/SpyShopContext.cs
+++ CoreCourse.Spyshop.Web/Data/SpyShopContext.cs
@@ -14,8 +14,38 @@ namespace CoreCourse.Spyshop.Web.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            DataSeeder.Seed(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .HasKey(e => e.Id);
 
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Name)
+                .HasMaxLength(50)               //max 50 chars
+                .IsRequired();                  //not nullable
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Price)
+                .HasColumnType("money");        //SQL datatype
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.PhotoUrl)
+                .HasMaxLength(250);             //max 250 chars
+
+            modelBuilder.Entity<Category>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.Name)
+                .HasMaxLength(50)               //max 50 chars
+                .IsRequired();                  //not nullable
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Products)       //category has multiple products...
+                .WithOne(p => p.Category)       //...which have only category
+                .IsRequired()                   //product must always have a category
+                .OnDelete(DeleteBehavior.Cascade); //automatically delete all products
+                                                    //when category is deleted
+
+            DataSeeder.Seed(modelBuilder);
             base.OnModelCreating(modelBuilder);
         }
     }
--- CoreCourse.Spyshop.Web/Migrations/20181014210652_ProdAndCatRelations.Designer.cs
+++ CoreCourse.Spyshop.Web/Migrations/20181014210652_ProdAndCatRelations.Designer.cs
@@ -4,14 +4,16 @@ using CoreCourse.Spyshop.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreCourse.Spyshop.Web.Migrations
 {
     [DbContext(typeof(SpyShopContext))]
-    partial class SpyShopContextModelSnapshot : ModelSnapshot
+    [Migration("20181014210652_ProdAndCatRelations")]
+    partial class ProdAndCatRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
