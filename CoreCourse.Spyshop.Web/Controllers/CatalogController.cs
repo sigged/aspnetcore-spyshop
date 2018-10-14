d97bac1ee7a7c04bfbf7d0fdd9e23f20a625e3cf
sigged <sderdeyn@gmail.com>
2018-10-14 22:59:42 +0200

    Modified CatalogController and View to display products from database

--- CoreCourse.Spyshop.Web/Controllers/CatalogController.cs
+++ CoreCourse.Spyshop.Web/Controllers/CatalogController.cs
@@ -1,37 +1,26 @@
-﻿using CoreCourse.Spyshop.Domain.Catalog;
+﻿using CoreCourse.Spyshop.Web.Data;
 using CoreCourse.Spyshop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CoreCourse.Spyshop.Web.Controllers
 {
     public class CatalogController : Controller
     {
-        List<Category> categories;
+        private readonly SpyShopContext context;
 
-        public CatalogController()
+        public CatalogController(SpyShopContext context)
         {
-            categories = new List<Category> {
-            new Category { Name = "General" },
-            new Category { Name = "Distraction" },
-            new Category { Name = "Communication" }
-        };
-
-            categories[0].Products = new List<Product> {
-            new Product{ Category = categories[0], Name = "Contactlenses", Price =13.95M,
-                Description = "The lenses allow to zoom up to 5 km." },
-            new Product{ Category = categories[0], Name = "Card game", Price =70.95M,
-                Description = "This card game can target a specific person an allows him to win or lose. Comes with a mobile app." }
-        };
-            categories[1].Products = new List<Product> {
-            new Product{ Category = categories[1], Name = "Holographic cushion", Price=55.45M,
-                Description = "A small button underneath displays a hologram, where you were previously seated" },
-        };
-            categories[2].Products = new List<Product>();
+            this.context = context;
         }
-        
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
+            var categories = await context.Categories
+                .Include(c => c.Products)
+                .ToListAsync();
+
             CatalogIndexVm viewmodel = new CatalogIndexVm();
             viewmodel.Categories = categories;
 
