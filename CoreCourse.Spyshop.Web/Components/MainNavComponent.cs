ea9d135651f2e4df9ee98f2482b326ad502c3530
sigged <sderdeyn@gmail.com>
2018-10-14 13:41:03 +0200

    Add layout, partials and components

---
+++ CoreCourse.Spyshop.Web/Components/MainNavComponent.cs
@@ -0,0 +1,39 @@
+﻿using CoreCourse.Spyshop.Web.ViewModels.Components;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreCourse.Spyshop.Web.Components
+{
+    [ViewComponent(Name = "MainNavigation")]
+    public class MainNavComponent : ViewComponent
+    {
+        private IEnumerable<MainNavLinkVm> publicLinks { get; set; }
+
+        public MainNavComponent()
+        {
+            publicLinks = new List<MainNavLinkVm>
+            {
+                new MainNavLinkVm{ Area=null, Controller="Home", Action="Index", Text="Start"},
+                new MainNavLinkVm{ Area=null, Controller="Catalog", Action="Index", Text="Products"},
+                new MainNavLinkVm{ Area=null, Controller="About", Action="Index", Text="About"},
+            };
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var navLinks = publicLinks;
+
+            foreach (var lnk in navLinks)
+            {
+                if (this.RouteData.Values["area"]?.ToString().ToLower() == lnk.Area?.ToLower() &&
+                    this.RouteData.Values["controller"]?.ToString().ToLower() == lnk.Controller.ToLower() &&
+                    this.RouteData.Values["action"]?.ToString().ToLower() == lnk.Action.ToLower())
+                {
+                    lnk.IsActive = true;
+                }
+            }
+            return await Task.FromResult<IViewComponentResult>(View(navLinks));
+        }
+    }
+}
